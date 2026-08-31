6230878cd5dfd700383385131995f4f7b818c2a2
Anmol <srivastavaanmol977@gmail.com>
2026-08-31 11:51:57 +0530

    feat: add Vercel URL to CORS

--- smarthire/src/main/java/com/smarthire/config/CorsConfig.java
+++ smarthire/src/main/java/com/smarthire/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:3000"
-
+                "http://localhost:3000",
+                "https://smarthire-backend-delta.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
